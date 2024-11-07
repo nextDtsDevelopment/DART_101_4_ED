5733855b97c3e9a3a8a2e93160e11dba25f0740f
nextDtsDevelopment <nextdts.development@gmail.com>
2024-11-06 19:19:06 -0500

    add: archivos

---
+++ 01_HOLA_MUNDO/JIMMY-POZO/CLASE/comentarios.dart
@@ -0,0 +1,5 @@
+void main() {
+// Comentario simple
+  print("Hola mundo");
+  print("Hola mundo");
+}
---
+++ 02_VARIABLES/JIMMY-POZO/CLASE/variables.dart
@@ -0,0 +1,25 @@
+String global = "Este es un valor global";
+
+void main() {
+  // var
+  var nombre = "Jimmy";
+
+  // late
+  // inicializarlo mas tarde
+  late String edad;
+  edad = "12";
+  print(edad);
+
+// Definir tipo de dato
+  String apellido = "Pozo";
+
+  /// Const y Final
+  ///
+  /// Const -> Asigna su espacio de menoria al compilar
+  ///
+  /// Final -> Se asigna su espacio de memoria al ejecutar
+  ///
+
+  const velocidadSonido = 250;
+  final diaSemana = "Lunes";
+}
